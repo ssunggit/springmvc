f8e8e92647c4b7bf4387958ea023ad2bfafa192d
ssunggit <gustjd68@gmail.com>
2022-08-09 16:36:28 +0900

    c

---
+++ src/main/java/kr/ac/kopo/hello/HelloController.java
@@ -0,0 +1,31 @@
+package kr.ac.kopo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	// localhost:8080/springmvc/hello/hello.do 요청이 들어오면
+	// hello() 메소드가 매핑
+	@RequestMapping("/hello/hello.do")
+	public ModelAndView hello() {
+		/*
+		 * ModelAndView 모델과 뷰를 둘 다 가짐 
+		 */
+		// jsp 주소를 찾아줌 _ hello/hello -> hello/hello.jsp 실행 (뷰)
+		ModelAndView mav = new ModelAndView("hello/hello");
+		// msg라는 이름으로 모델 추가 -> jsp가 msg를 사용 할 수 있다.
+		mav.addObject("msg", "Hello Spring MVC!!");
+		
+		return mav;
+	}
+}
+
+/*
+ * 리턴타입
+ * 1. ModelAndView
+ * 2. String (jsp 주소)
+ * 3. void
+ * 4. json string (jsp 주소가 아닌 것) 
+ */
---
+++ src/main/webapp/WEB-INF/jsp/hello/hello.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>hello</title>
+</head>
+<body>
+hello.jsp입니다
+<br>
+서버에서 받은 Message : ${ msg }
+</body>
+</html>
